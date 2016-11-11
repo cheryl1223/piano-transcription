d8cae74803771b0241730c6a5e3b9303ab6e7a14
jerica0527 <jerica0527@gmail.com>
2016-11-11 14:19:30 +0800

    refresh workspace

--- workspace/envelope.py
+++ workspace/envelope.py
@@ -3,7 +3,7 @@ import scipy.signal.signaltools as sigtool
 import numpy, matplotlib.pyplot as plt,  librosa, IPython.display as ipd, urllib, os.path
 
 x = linspace(0,5,1e4)
-s, fs = librosa.load('/home/cheryl/FYP/test/C4.wav', sr=44100)
+s, fs = librosa.load('C4.wav', sr=44100)
 env = numpy.abs(sigtool.hilbert(s)) # hilbert(s) actually returns the analytical signal
 
 plot(x,s,label='Time Signal')
