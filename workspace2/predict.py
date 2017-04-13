60f2d61c22aadd06f6cc10a9ffc1eacec53440cf
cheryl1223 <xieyuxiao@outlook.com>
2017-04-13 17:42:07 +0800

    draft

--- workspace2/predict.py
+++ workspace2/predict.py
@@ -1,18 +1,20 @@
-from train import NeuralNetwork
+from keras.models import load_model
 import numpy as np
 import sys,os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
 filename = sys.argv[1].split(".")[0]
 os.system("python3 transform.py %s.wav"%filename)
-nn = NeuralNetwork()
-nn.load('nn')
-print(nn.model.summary())
+nn = load_model('nn.h5')
+print(nn.summary())
+
 X = np.loadtxt("%s_transform.txt"%filename)
 for i in range(len(X)):
     X[i] = X[i].reshape(-1,180)
 print(nn.predict(X).shape)
 
-y = nn.model.predict(X)
-y[y > 0.4] = 1
-y[y <= 0.4] = 0
+y = nn.predict(X)
+y[y > 0.95] = 1
+y[y <= 0.95] = 0
 y.astype(int)
 np.savetxt("result.txt", y, fmt = "%d")
